a5a2772505348383509dbf7233bc4f8b90f0492b
alejandra.diaz <alejandra.diaz@ixcomercio.com>
2023-04-14 20:12:21 -0400

    Material Sesión 7

---
+++ M2S7 - C-Javascript/assets/js/practica.js
@@ -0,0 +1,9 @@
+let numero = 1000
+
+if(numero >=0 && numero < 10){
+    console.log('Es unidad')
+}else if(numero >= 10 && numero < 100){
+    console.log('Es decena')
+}else if(numero >= 100 && numero < 1000){
+    console.log('Es centena')
+}
